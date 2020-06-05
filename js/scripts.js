25a900f490cd50b749f8c241cf66115ad15987c3
Emma Hellman <ehh801@icloud.com>
2020-06-05 09:49:28 -0700

    add more combo variables

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,26 @@ $(document).ready(function() {
     const celebrity = $("select#celebrity").val();
     const drink = $("select#drink").val();
 
+    //All correct for Javascript
     const javascript1 = food === "burritos" && languages === "scripting" && band === "pinkfloyd" && celebrity === "britneyspears" && drink === "coffee";
+    //Four correct for Javascript
     const javascript2 = food === "burritos" && languages === "scripting" && band === "pinkfloyd" && celebrity === "britneyspears" && drink === "tea";
     const javascript3 = food === "burritos" && languages === "scripting" && band === "pinkfloyd" && celebrity === "britneyspears" && drink === "soda";
     const javascript4 = food === "burritos" && languages === "scripting" && band === "pinkfloyd" && celebrity === "willsmith" && drink === "coffee";
     const javascript5 = food === "burritos" && languages === "scripting" && band === "pinkfloyd" && celebrity === "mileycyrus" && drink === "coffee";
-    const javascript5 = food === "burritos" && languages === "scripting" && band === "linkinpark" && celebrity === "britneyspears" && drink === "coffee";
-    const javascript5 = food === "burritos" && languages === "scripting" && band === "thebeatles" && celebrity === "britneyspears" && drink === "coffee";
+    const javascript6 = food === "burritos" && languages === "scripting" && band === "linkinpark" && celebrity === "britneyspears" && drink === "coffee";
+    const javascript7 = food === "burritos" && languages === "scripting" && band === "thebeatles" && celebrity === "britneyspears" && drink === "coffee";
+    const javascript8 = food === "burritos" && languages === "objectoriented" && band === "pinkfloyd" && celebrity === "britneyspears" && drink === "coffee";
+    const javascipt9 
 
 
-    if (javascript1 || javascript2 || javascript3 || javascript4 || javascript5) {
+    if (javascript1 || javascript2 || javascript3 || javascript4 || javascript5 || javascript6 || javascript7 ) {
       $("#favoritelanague").text("javascript");
       $("#languagemessage").show();
     } else {
       $("#favoritelanague").text("something");
       $("#languagemessage").show();
-    }
+    } 
 
     event.preventDefault();
 
